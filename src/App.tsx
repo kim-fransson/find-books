9d1c8839f49fe9a718753a0839a15701480991c5
Kim Fransson <kim.fransson.22@gmail.com>
2025-05-20 15:16:18 +0200

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-svh flex flex-col">
+      <header className="min-h-36 bg-pattern-mobile lg:bg-pattern-desktop"></header>
+      <main>hej</main>
+      <footer className="mt-auto min-h-36 bg-pattern-mobile lg:bg-pattern-desktop"></footer>
+    </div>
+  );
+}
+
+export default App;
